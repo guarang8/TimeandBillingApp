838a35cd1710ab01930a9f8a7f4145075e56aca6
manishmsn <manish.guaranga@gmail.com>
2012-01-05 01:18:34 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeAndBillingApp::Application.config.secret_token = '99b3051e53f2ca1867ec8153425edbf5fa34cd41c3379d271d51cac97dca7bd572bd7f30258ef3dd5c79d13602bc594b39c4aaeb3286acb10ab5701787024b5d'
